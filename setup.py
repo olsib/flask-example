9275c02340b0d6d7fcd20051a6f6682d1a61c963
root <root@ip-10-35-225-7.mdevlab.com>
2016-08-15 12:59:13 -0400

    asdf

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='ListIDs',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Olsi Birbo',
+      author_email='olsi.birbo@ihsmarkit.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
       install_requires=['Flask>=0.7.2', 'MarkupSafe'],
       )
