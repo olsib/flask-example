c5555beda140b851f0c2c4573b013abda923e466
root <root@ip-10-35-225-7.mdevlab.com>
2016-08-15 13:31:50 -0400

    asdf

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='ListIDs',
       author='Olsi Birbo',
       author_email='olsi.birbo@ihsmarkit.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe', 'python-dev'],
+      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
       )
