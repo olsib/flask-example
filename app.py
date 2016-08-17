75c407c9d241a5305cc89331d2334cef8ffdc1eb
root <root@ip-10-35-225-7.mdevlab.com>
2016-08-17 07:46:57 -0400

    added few templates

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask
 from flask import g
 from flask import Response
 from flask import request
-from flask import render_template, jsonify
+from flask import render_template, jsonify, redirect
 
 
 app = Flask(__name__)
@@ -17,7 +17,7 @@ def db_connect():
   g.conn = pymysql.connect(host='172.30.49.196',
                               user='root',
                               passwd='Welcome1',
-                              db='testdb')
+                              db='linkid_db')
   g.cursor = g.conn.cursor()
 
 @app.after_request
@@ -30,29 +30,73 @@ def query_db(query, args=(), one=False):
   g.cursor.execute(query, args)
   rv = [dict((g.cursor.description[idx][0], value)
   for idx, value in enumerate(row)) for row in g.cursor.fetchall()]
+  print rv
   return (rv[0] if rv else None) if one else rv
 
 @app.route("/")
 def hello():
-  return "Hello World!"
-
+#  return "Whazzuppp!"
+  return redirect("127.0.0.1:5000/names")
+ 
 @app.route("/names", methods=['GET'])
 def names():
-  result = query_db("SELECT firstname,lastname FROM testdb.Persons")
-  data = json.dumps(result)
-  new_obj = json.loads(data)
-  return render_template('index.html', json = data, obj = new_obj)
+  result_current = query_db("SELECT name, linkid_current FROM linkid_db.nodes_current")
+  data_current = json.dumps(result_current)
+  new_obj_current = json.loads(data_current)
+  for i in range(0,4):
+    print new_obj_current[i]['name']
+    if new_obj_current[i]['name'] == 'dist1':
+      global dist1_current
+      dist1_current = new_obj_current[i]['linkid_current']
+    elif new_obj_current[i]['name'] == 'dist2':
+      global dist2_current
+      dist2_current = new_obj_current[i]['linkid_current']
+    elif new_obj_current[i]['name'] == 'sync1':
+      global sync1_current
+      sync1_current = new_obj_current[i]['linkid_current']
+    else:
+      global sync2_current
+      sync2_current = new_obj_current[i]['linkid_current']
+  print dist1_current,dist2_current,sync1_current,sync2_current
 
-  #resp = Response(data, status=200, mimetype='application/json')
-  #return resp
+  result_latest = query_db("SELECT name, linkid_latest FROM linkid_db.nodes")
+  data_latest = json.dumps(result_latest)
+  new_obj_latest = json.loads(data_latest)
+  for j in range(0,4):
+    print new_obj_latest[j]['name']
+    if new_obj_latest[j]['name'] == 'dist1':
+      global dist1_latest
+      dist1_latest = new_obj_latest[j]['linkid_latest']
+    elif new_obj_current[j]['name'] == 'dist2':
+      global dist2_latest
+      dist2_latest = new_obj_latest[j]['linkid_latest']
+    elif new_obj_current[j]['name'] == 'sync1':
+      global sync1_latest
+      sync1_latest = new_obj_latest[j]['linkid_latest']
+    else:
+      global sync2_latest
+      sync2_latest = new_obj_latest[j]['linkid_latest']
+  print dist1_latest,dist2_latest,sync1_latest,sync2_latest
+  print data_current,data_latest
+  both_obj = data_current + data_latest
+  print both_obj
+
+#  return render_template('index.html', json = data_current, obj = both_obj)
+  if dist1_latest >= dist1_current and dist2_latest >= dist2_current:
+    return render_template('indexgreen.html', title = 'Home', dist1_current=dist1_current, dist2_current=dist2_current)
+  elif dist1_latest <= dist1_current or dist2_latest <= dist2_current:
+    return render_template('indexred.html', title = 'Home', dist1_current=dist1_current, dist2_current=dist2_current)
+  else:
+    return render_template('indexyellow.html', title = 'Home', dist1_current=dist1_current, dist2_current=dist2_current)
 
 @app.route("/add", methods=['POST'])
 def add():
   req_json = request.get_json()
-  g.cursor.execute("INSERT INTO test.name (firstname, lastname) VALUES (%s,%s)", (req_json['firstname'], req_json['lastname']))
+  g.cursor.execute("UPDATE TABLE linkid_db.nodes set cr(firstname, lastname) VALUES (%s,%s)", (req_json['firstname'], req_json['lastname']))
   g.conn.commit()
   resp = Response("Updated", status=201, mimetype='application/json')
   return resp
 
 if __name__ == "__main__":
+  app.debug = True
   app.run(host='0.0.0.0', port=5005)
